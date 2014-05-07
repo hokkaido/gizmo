14a836482cfeef1fd765692bfc894cad7c8e0bf2
hokkaido <hello@kimchi.io>
2014-05-07 14:23:26 +0200

    Moving the sub-interfaces / extensions of Graph to graph.clj

--- src/gizmo/graph.clj
+++ src/gizmo/graph.clj
@@ -51,3 +51,61 @@
 (defn shutdown!
   [^Graph graph]
   (.shutdown graph))
+
+;; TransactionalGraph
+
+;;(defn shutdown!
+;;  [^TransactionalGraph graph]
+;;  (.shutdown graph))
+
+(defn commit!
+  [^TransactionalGraph graph]
+  (.shutdown commit))
+
+(defn rollback!
+  [^TransactionalGraph graph]
+  (.shutdown rollback))
+
+;; ThreadedTransactionalGraph
+
+(defn new-transaction!
+  [^ThreadedTransactionalGraph graph]
+  (.newTransaction graph)))
+
+;; IndexableGraph
+
+(defn create-index!
+  [^IndexableGraph graph indexName indexClass params]
+  (.createIndex graph indexName indexClass (into-array String params)))
+
+(defn get-index
+  [^IndexableGraph graph indexName indexClass]
+  (.getIndex graph indexName indexClass))
+
+(defn get-indices
+  [^IndexableGraph graph]
+  (.getIndices graph))
+
+(defn drop-index!
+  [^IndexableGraph graph]
+  (.dropIndex graph))
+
+;; KeyIndexableGraph
+
+(defn drop-key-index!
+  [^KeyIndexableGraph graph key elementClass]
+  (.dropKeyIndex graph key elementClass))
+
+(defn create-key-index!
+  [^KeyIndexableGraph graph key elementClass indexParameters]
+  (.createKeyIndex graph key elementClass (into-array ^Parameter indexParameters)))
+
+(defn get-indexed-keys
+  [^KeyIndexableGraph graph elementClass]
+  (.getIndexedKeys graph elementClass))
+
+;; MetaGraph
+
+(defn get-raw-graph
+  [^MetaGraph graph]
+  (.getRawGraph graph))
